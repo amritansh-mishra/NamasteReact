fab1eb4cd3059f0f6232ce8b37c9f41bed035fd3
amritansh-mishra <mamritansh1911@gmail.com>
2025-02-26 11:41:55 +0530

    episode-1

---
+++ react.js
@@ -0,0 +1,32 @@
+ const heading = React.createElement("h1" , {} , "Hello World React" );
+ 
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ 
+ // root.render(heading);
+
+
+ /**                                   HOW TO CREATE NESTED ELEMENT
+  * 
+  * 
+  * 
+  * <div id="parent">
+  *      <div id=" child">
+  *           <h1>"I'm here</h1> 
+  *     </div>
+  * </div>
+  * 
+  * 
+  * 
+  * 
+  */
+
+ const parent = React.createElement("div",
+     {id: "parent"} ,
+    React.createElement("div", 
+        {id:"child"},
+           React.createElement("h1", {} , "I'm here" )));
+ console.log(parent);
+
+ root.render(parent);
+
+ 
