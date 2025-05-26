e197561b615b99c9b88dc61d9a7938c9a01fae0c
amritansh-mishra <mamritansh1911@gmail.com>
2025-05-26 17:31:24 +0530

    src

---
+++ src/components/HeadingComponent.js
@@ -0,0 +1,34 @@
+//JSX
+
+export const Title = () => (
+  <a href="/">
+
+<img 
+ className="logo"
+ alt= "logo" 
+ src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLDO71vDJJ3V3gQrmwYj2ANviQAdX9gIqw0g&s">
+ </img>
+
+  </a>
+);
+ 
+
+//React Functional component
+const HeadingComponent = () => {
+  return (
+    <div className="header">
+      <Title />                  
+      <div className="nav-items">
+        <ul>
+          <li> Home</li>
+          <li> About</li>
+          <li> Contact</li>
+          <li> Cart</li>
+        </ul>
+      </div>
+    </div>
+    
+  );
+};
+
+export default HeadingComponent;
--- src/react.js
+++ src/react.js
@@ -39,39 +39,11 @@ root.render(heading);
 
 import React from "react";
 import ReactDOM, {createRoot} from "react-dom/client";
+//export default
+import HeadingComponent from "./components/HeadingComponent";
+// exporting by name 
+import {Title} from "./components/HeadingComponent";
 
-//JSX
-
-const Title = () => (
-  <a href="/">
-
-<img 
- className="logo"
- alt= "logo" 
- src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLDO71vDJJ3V3gQrmwYj2ANviQAdX9gIqw0g&s">
- </img>
-
-  </a>
-);
- 
-
-//React Functional component
-const HeadingComponent = () => {
-  return (
-    <div className="header">
-      <Title />                  
-      <div className="nav-items">
-        <ul>
-          <li> Home</li>
-          <li> About</li>
-          <li> Contact</li>
-          <li> Cart</li>
-        </ul>
-      </div>
-    </div>
-    
-  );
-};
 
 const restrolist =[
                   {
