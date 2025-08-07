3be95893b87e9444325114d1951b45d73cce6bf5
amritansh-mishra <mamritansh1911@gmail.com>
2025-08-07 16:43:30 +0530

    class Component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import user from "./user";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>We are a team of passionate developers.</p>
+      <user />
+    </div>
+  );
+}   
+
+export default AboutUs;
---
+++ src/components/user.js
@@ -0,0 +1,14 @@
+const user = () => {
+  return (
+    <div className="user">< /div>
+      <h1>Amrit</h1>
+      <h2>React Developer</h2>
+    </div>
+  );
+};
+
+export default user;
+
+// This component is a simple functional component that returns a div with a class name "user" and contains an h1 and h2 element.
+// The h1 element displays the name "Amrit" and the h2 element displays the designation "React Developer".
+// It is exported as the default export of the module, allowing it to be imported and used in other files.
